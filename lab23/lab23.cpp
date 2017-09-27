02631e9a15b5defed54ee4332d2826d4b78fedaf
Elias Naron <enaron001@student.butte.edu>
2017-09-27 04:39:52 +0000

    lab submission 2.3

---
+++ lab23/lab23.cpp
@@ -0,0 +1,62 @@
+//Elias Naron
+//9/21/2017
+//guessing game
+
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+/* This is a swap function ,which to my understanding is just
+setting kind of a value place holder on your arguments so you can toss them
+between functions? Im not really sure, but it didn't break my program so i am
+happy.... */
+
+
+void swap (int& minnum, int& maxnum)
+{
+    int swaap;
+    swaap = maxnum;
+    swaap = minnum;
+    
+}
+/* This is my real function that does the math! I set this here so i can call
+it in my main later on */
+
+void NumberGenerator(int& maxnum ,int& minnum)
+{
+   
+
+    swap (minnum, maxnum);
+    srand(time(0));
+    int difference = (maxnum - minnum) + 1 ;
+    int GeneratedNumber = rand() % difference + minnum;
+    cout<<GeneratedNumber<<endl;
+    
+    
+}
+// Calling function and getting user input!
+int main()
+{
+    int minnum;
+    int maxnum;
+ 
+    cout<<"▲Fun random number calulator!▲"<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"Please input your \"minimum\" possible number."<<endl;
+    cin>>minnum;
+    cout<<endl;
+    cout<<"Please input your \"maximum\" possible number."<<endl;
+    cin>>maxnum;
+    
+    cout<<"Your random number between "<<minnum<<" and "<<maxnum<<" is :"<<endl;
+    NumberGenerator(maxnum, minnum);
+    cout<<"Wow!"<<endl;
+    
+
+
+    
+}
+
