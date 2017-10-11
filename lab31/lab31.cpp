8deaf385ab4b54bfe1245bd2616bdf0f3a1b5b38
Elias Naron <enaron001@student.butte.edu>
2017-10-11 18:31:10 +0000

    lab31 submission

---
+++ lab31/lab31.cpp
@@ -0,0 +1,173 @@
+//Elias Naron  
+//10/10/2017
+
+
+
+//includes! The ones that we need of course
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+//The first step : defining our class
+ int main () 
+ {
+     
+     int    ppl_on_Plan_;
+     int    phone_Type_smart_;
+     int    phone_Type_dumb_;
+     int    num_Gigs_;
+     int    sprint_unlimited_;
+     int    verizon_unlimited_;
+     
+     
+    cout<<" Let's Get You and/or Your Family a Phone Plan!"<<endl;
+    count<<endl;
+    
+    cout<<" First things first, how many people are going need a line? (must be less than or equal to 10"<<endl;
+    
+    count<<endl;
+    cin>>ppl_on_Plan_;
+    count<<endl;
+    
+    cout<<" Great!"<<endl;
+    cout<<" How many gigs of data are you going to need? (PLEAE ENTER: 0 , 1 , 3, 6 , 10 , 16, 25, 99 for unlimited "<<endl;
+    
+    count<<endl;
+    cin>>num_Gigs_;
+    count<<endl;
+    
+    cout<<"Fantastic!"<<endl;
+    cout<"How many of these lines are (Smart Phones)?"<<endl;
+    
+    
+    count<<endl;
+    cin>>phone_Type_smart_;
+    count<<endl;
+    
+    
+    cout<<"Terrelse ific!"<<endl;
+    cout<"How many of these lines are (Dumb Phones)?"<<endl;
+    
+    
+    count<<endl;
+    cin>>phone_Type_dumb_;
+    count<<endl;
+    
+    if (num_Gigs_ == 0)
+    {
+    ATT Family Planss
+
+    *$20 per phone, $10 per tablet
+        
+    }
+    else if (num_Gigs_ == 1) 
+    {
+        
+    ATT Family Plans
+
+    *$20 per phone, $10 per tablet
+
+    *$30 for 1 GB of Data
+        
+    }
+    
+        
+    else if (num_Gigs_ == 3)
+    {
+        
+    }
+    else if (num_Gigs_ == 6)
+    {
+        
+    }
+    else if (num_Gigs_ == 16)
+    {
+        
+    }
+    else if (num_Gigs_ == 25){
+        
+    }
+    else if (num_Gigs_ == 25)
+    {
+        
+    }
+
+    
+    
+    
+    
+    
+//     ATT Unlimited Plus Plan
+
+// * $90/mo for the first line, $145/mo for 2 lines, an additional $20/mo up to 10 max for phones, $10/mo per tablet
+
+// ATT Unlimited Choice Plan
+
+// * $60/mo for the first line, $115/mo for 2 lines, an additional $20/mo up to 10 max for phones, $10/mo per tablet.
+
+// ATT Family Plans
+
+// *$20 per phone, $10 per tablet
+
+// *$30 for 1 GB of Data
+
+// *$40 for 3 GB of Data
+
+// *$60 for 6 GB of Data
+
+// *$80 for 10 GB of Data
+
+// *$90 for 16 GB of Data
+
+// *$110 for 25 GB of Data
+
+ 
+
+// Verizon Unlimited
+
+// *$80 for the first line, $110 for more than 1 line, $20 per phone starting at the first, $10 per tablet
+
+ 
+
+ 
+
+// Sprint Unlimited
+
+// *$60 for the 1st line, $40 for the second line, Additional $30/mo per line
+
+ 
+
+// Sprint 2GB Plan
+
+// *$40 per month per line
+
+// Determine the best plan for the user based on their needs.
+     
+     
+     
+     
+     
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+HOW MANY PEOPLE ON PLAN
+
+What kind of phones/tablets
+
+HOW MANY GIGS
