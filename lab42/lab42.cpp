7d15921f1c008827cbad7480ca16af52779c4d41
Elias Naron <enaron001@student.butte.edu>
2017-11-08 06:54:22 +0000

    lab42 submission

--- lab42/lab42.cpp
+++ lab42/lab42.cpp
@@ -1,102 +1,76 @@
 //Elias Naron   
-//11/2/2017
+//11/07/2017
 
 
-#include <iostream>
+#include <iostream>     //Gotta have the includes!                                                  
 #include <string>
-
 using namespace std;
 
-int main () {
+int main(){  //main starts here
+                                                      
+    string goods_[10];             // This is my first array that stores buyable items
+      goods_[9]= "Diamond      ";
+      goods_[8]= "Silver        "; // this shows the item within each tier of the array
+      goods_[7]= "Platinum      ";
+      goods_[6]= "Titanium      ";
+      goods_[5]= "Onyx          ";
+      goods_[4]= "Cobalt        ";
+      goods_[3]= "Ruby          ";
+      goods_[2]= "Emerald       ";
+      goods_[1]= "Amethyst      ";
+      goods_[0]= "Topaz         ";
+    double price_[10] ;   // second array 
+       price_[9] = 999.99;//second set of values per tier
+       price_[8] = 899.99;
+       price_[7] = 799.99;
+       price_[6] = 699.99;
+       price_[5] = 599.99;
+       price_[4] = 499.99;
+       price_[3] = 399.99;
+       price_[2] = 299.99;
+       price_[1] = 199.99;
+       price_[0] = 99.99;
+    int stock_[10]; //third set (final set)
+        stock_[9] = 2;//all with the same value but i didnt know how to make it the same for each
+        stock_[8] = 2;// without re-stating them
+        stock_[7] = 2;
+        stock_[6] = 2;
+        stock_[5] = 2;
+        stock_[4] = 2;
+        stock_[3] = 2;
+        stock_[2] = 2;
+        stock_[1] = 2;
+        stock_[0] = 2;
+        
+        
+    double cart_total_= 0;                                                     
+    char shopping_ = 'y' ;// these continue loops were hard for me and for the longest time i couldnt get it to start!
+                          // so i found out you have to initialize it to y so that the program can start from a technical continue
     
-    int     NumItems = 10;
-    string  Items_[NumItems] ;
-    double  Price_[NumItems] ;
-    int UserInput = 0;
-    double Cart = 0 ;
-    //string StillShopping = ' ';
-    
-    
-    Items_[10]               ; Price_[10]        ;
-    Items_[9] = "Diamond   "; Price_[9] = 9.99  ;
-    Items_[8] = "Gold bar   "; Price_[8] = 8.99  ;
-    Items_[7] = "Steak      "; Price_[7] = 7.99  ;
-    Items_[6] = "Silver Bar "; Price_[6] = 6.99  ;
-    Items_[5] = "Iron Bar   "; Price_[5] = 5.99  ;
-    Items_[4] = "Orb of Zot "; Price_[4] = 4.99  ;
-    Items_[3] = "Emerald    "; Price_[3] = 3.99  ;
-    Items_[2] = "Bone       "; Price_[2] = 2.99  ;
-    Items_[1] = "Chicken    "; Price_[1] = 1.99  ;
-    Items_[0] = "Dust       "; Price_[0] = 0.99  ;
-    
-    
-     
-    for (int i = 0; i <NumItems; i++) {
-        cout<< i+1 << ". "<< Items_[i]<<" -   "<<Price_[i]<<endl;
-    }
-    cout<<"pick item # want..."<<endl;
-    cin>>UserInput;
-    
-    if (UserInput == 0) {
+    while ( shopping_ == 'Y' || shopping_ == 'y'){     // continue shopping loop :)                  
+        int user_input_ = 0;
         
-        Cart = Price_[0];
+        for ( int i = 0; i < 10; i++ ){         // i had to see how this was done but it functions as a way to index the items for the cart                               
+        cout << i + 1 << "~ " << goods_[i] << "--  $" << price_[i] << endl;
+        }
         
-    }
-    if (UserInput == 1) {
+        cout << "Select another item that you would like to add to your cart: "; //grabbing user input where everything runrs
+        cin >> user_input_;                                                       
+        if (stock_[user_input_ - 1] <= 0){
+            cout << "Unfortunately that item is sold out; Please select another (different) item: ";
+            cin >> user_input_;
+        }
+        stock_ [user_input_ - 1] = stock_[user_input_ - 1] - 1;//this was tricky but its how i remove the item from stock it has to be its own array
         
-        Cart = Price_[1];
+        cout << "You have added :  " << goods_[user_input_ - 1] << "to your cart."<<endl; 
         
-    }
-    if (UserInput == 2) {
+        cart_total_ = cart_total_ + price_[user_input_ - 1]; // this adds the item to the total in the cart
         
-        Cart = Price_[2];
-        
-    }
-    if (UserInput == 3) {
-        
-        Cart = Price_[3];
-        
-    }
-    if (UserInput == 4) {
-        
-        Cart = Price_[4];
-        
-    }
-    
-    if (UserInput == 5) {
-        
-        Cart = Price_[5];
-        
-    }
-    
-    if (UserInput == 6) {
-        
-        Cart = Price_[6];
-        
-    }
-    
-    if (UserInput == 7) {
-        
-        Cart = Price_[7];
-        
+        cout << "Continue Shopping ? (Y)es or (N)o : ";                
+        cin >> shopping_; //input for the continuation loopage
     }
 
-    if (UserInput == 8) {
-        
-        Cart = Price_[8];
-        
-    }
-     
-    if (UserInput == 9) {
-        
-        Cart = Price_[9];
-        
-    }
-     
-    
-    //while ( StillShopping == 'Y' || StillShopping == 'y' || StillShopping == "yes" ||StillShopping == "Yes") {
-        
-    //}
-    cout<<Cart;
-    
+     cout << "Your total cart value is : " << cart_total_; // yay all done!                           
+
+    return(0);
 }
