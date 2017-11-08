543bf0e1dede11ce22ba2ec36d177a47ee97239a
Elias Naron <enaron001@student.butte.edu>
2017-11-08 06:58:21 +0000

    lab43 submission

---
+++ lab43/lab43.cpp
@@ -0,0 +1,35 @@
+//Elias Naron   
+//11/07/2017
+
+
+#include <iostream>                                                  
+#include <string>
+using namespace std;
+
+
+class thing {
+    public:
+        thing();
+        thing( string item, int num, double price);
+        
+        void SetName( string name);
+        void SetInventory( int num);
+        void SetPrice( double price);
+        
+        string ();
+        int ();
+        double();
+        void Print();
+    
+    private:
+        variables
+};
+
+FUNCTIONS
+
+int main(){
+    
+    
+    
+    return 0;
+}
