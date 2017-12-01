6d356f5c97987cdb4e881df995f71591b77d2db7
Elias Naron <enaron001@student.butte.edu>
2017-12-01 04:08:31 +0000

    lab52 submission

---
+++ lab52/lab52.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+/*
+ *  This class is to show how to
+ *  break our class into an interface and implementation
+ *  file. It will set a member variable to 0 on initialization
+ *  and output it using the Output() function.
+ */
+
---
+++ lab52/main.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "myclass.h"
+using namespace std;
+
+int randomNumber();
+
+
+
+
